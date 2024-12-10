6c93eea2af6e043430f357db9ecacc15108db0b6
karim.mammadzada <karim.mammadzada@unibank.az>
2024-12-10 17:01:21 +0400

    note added

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -23,7 +23,30 @@ class MainActivity : AppCompatActivity() {
 
         initializeDependency()
     }
+    /**
+     * Way 1.
+     * presentation module implements data module.
+     * Thus presentation is able to create MusicPlayerRepositoryImpl and provide to domain
+     *
+     * */
+//    private fun initializeDependency(){
+//        val repository: MusicPlayerRepository = MusicPlayerRepositoryImpl()
+//        val useCase = PlayMusicUseCase(repository)
+//        viewModel = MainViewModel(useCase)
+//        val result = viewModel.playMusic()
+//        val view = findViewById<TextView>(R.id.textview)
+//        view.setText(result)
+//    }
 
+    /**
+     * Way 2.
+     * presentation module implements domain module.
+     * domain needs mapped form of data module's class
+     * this is done in MainApplication because MainApplication
+     * is able to see all modules.
+     * MainApplication serves dependency handler in this scenerio - same as Hilt
+     *
+     * */
     private fun initializeDependency() {
         val useCase = PlayMusicUseCase.getInstance()
         viewModel = MainViewModel(useCase)
